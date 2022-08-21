bb08863ad28e0f1a19dcb605be12f6dcd53df84b
あちゃぴ <67042875+achapi@users.noreply.github.com>
2022-08-21 18:11:50 +0900

    Update 最大公約数,最小公倍数.py

--- 最大公約数,最小公倍数.py
+++ 最大公約数,最小公倍数.py
@@ -1,11 +1,8 @@
 from functools import reduce
 import math
 
-def gcd(*numbers):
-    return reduce(math.gcd, numbers)
+def gcd(*numbers):return reduce(math.gcd, numbers)
 
-def my_lcm_base(x, y):
-    return (x * y) // math.gcd(x, y)
+def _lcm(x,y):return (x * y) // math.gcd(x, y)
 
-def lcm(*numbers):
-    return reduce(my_lcm_base, numbers, 1)
+def lcm(*numbers):return reduce(_lcm, numbers, 1)
