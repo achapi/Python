3482ddb19a635db0dded87f2962a1d26ea65b0f1
あちゃぴ <67042875+achapi@users.noreply.github.com>
2022-08-29 00:51:06 +0900

    Create 遅延セグ木(区間更新用).py

---
+++ 遅延セグ木(区間更新用).py
@@ -0,0 +1,85 @@
+class LazySegTree_RUQ:
+    def __init__(self,init_val,segfunc,ide_ele):
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1<<(n-1).bit_length()
+        self.tree = [ide_ele]*2*self.num
+        self.lazy = [None]*2*self.num
+        for i in range(n):
+            self.tree[self.num+i] = init_val[i]
+        for i in range(self.num-1,0,-1):
+            self.tree[i] = self.segfunc(self.tree[2*i],self.tree[2*i+1])
+    def gindex(self,l,r):
+        l += self.num
+        r += self.num
+        lm = l>>(l&-l).bit_length()
+        rm = r>>(r&-r).bit_length()
+        while r>l:
+            if l<=lm:
+                yield l
+            if r<=rm:
+                yield r
+            r >>= 1
+            l >>= 1
+        while l:
+            yield l
+            l >>= 1
+    def propagates(self,*ids):
+        for i in reversed(ids):
+            v = self.lazy[i]
+            if v is None:
+                continue
+            self.lazy[i] = None
+            self.lazy[2*i] = v
+            self.lazy[2*i+1] = v
+            self.tree[2*i] = v
+            self.tree[2*i+1] = v
+    def update(self,l,r,x):
+        ids = self.gindex(l,r)
+        self.propagates(*self.gindex(l,r))
+        l += self.num
+        r += self.num
+        while l<r:
+            if l&1:
+                self.lazy[l] = x
+                self.tree[l] = x
+                l += 1
+            if r&1:
+                self.lazy[r-1] = x
+                self.tree[r-1] = x
+            r >>= 1
+            l >>= 1
+        for i in ids:
+            self.tree[i] = self.segfunc(self.tree[2*i], self.tree[2*i+1])
+    def query(self,l,r):
+        ids = self.gindex(l,r)
+        self.propagates(*self.gindex(l,r))
+        res = self.ide_ele
+        l += self.num
+        r += self.num
+        while l<r:
+            if l&1:
+                res = self.segfunc(res,self.tree[l])
+                l += 1
+            if r&1:
+                res = self.segfunc(res,self.tree[r-1])
+            l >>= 1
+            r >>= 1
+        return res
+#返す値
+def segfunc(x, y):
+    #return max(x, y)
+    #return min(x, y)
+    #return x+y
+    #return x*y
+
+#単位元
+#ide_ele = -float('inf')
+#ide_ele = float('inf')
+#ide_ele = 0
+#ide_ele = 1
+
+#update(l, r, x): 区間[l, r)の値をxに更新
+#query(l, r): 区間[l, r)をsegfuncしたものを返す
+seg = LazySegTree_RUQ([], segfunc, ide_ele)
