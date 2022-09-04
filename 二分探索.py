cf7968298e032f1213578b9c8914829f1e0cbf16
あちゃぴ <67042875+achapi@users.noreply.github.com>
2022-09-04 18:53:34 +0900

    Update 二分探索.py

--- 二分探索.py
+++ 二分探索.py
@@ -1,15 +1,9 @@
-def validate(n):
-    return 
+def validate(x):
+  return 
 
-def binarysearch():
-    left = 1
-    right = 10 ** 9
-    r=0
-    while left <= right:
-        mid = (left+right) // 2
-        if validate(mid):
-            r = mid
-            left = mid + 1
-        else:
-            right = mid -1
+def binarysearch(left=1,right=10**18,r=0):
+    while left<=right:
+        mid=(left+right)//2
+        if validate(mid):r=mid;left=mid+1
+        else:right=mid-1
     return r
