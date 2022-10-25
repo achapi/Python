2affa178b8ccefc0540f80e835364b26f5814f5e
あちゃぴ <67042875+achapi@users.noreply.github.com>
2022-10-25 16:30:55 +0900

    Update 遅延セグ木(区間加算用).py

--- 遅延セグ木(区間加算用).py
+++ 遅延セグ木(区間加算用).py
@@ -78,6 +78,6 @@ def segfunc(x, y):
 #ide_ele = 0
 #ide_ele = 1
 
-#update(l, r, x): 区間[l, r)の値にxを加算
-#add(l, r): 区間[l, r)をsegfuncしたものを返す
+#add(l, r, x): 区間[l, r)の値にxを加算
+#query(l, r): 区間[l, r)をsegfuncしたものを返す
 seg = LazySegTree_RAQ([], segfunc, ide_ele)
