80ef7c111176c411a20a00ddf4d32857fa531037
あちゃぴ <67042875+achapi@users.noreply.github.com>
2022-12-10 02:28:54 +0900

    Update bisect.py

--- bisect.py
+++ bisect.py
@@ -2,24 +2,24 @@ from bisect import*
 def index(a, x):
     i = bisect_left(a, x)
     if i != len(a) and a[i] == x:return i
-    raise ValueError
+    return None
 def lt(a, x):
     # x未満
     i = bisect_left(a, x)
     if i:return a[i-1]
-    raise ValueError
+    return None
 def le(a, x):
     # x以下
     i = bisect_right(a, x)
     if i:return a[i-1]
-    raise ValueError
+    return None
 def gt(a, x):
     # x超
     i = bisect_right(a, x)
     if i != len(a):return a[i]
-    raise ValueError
+    return None
 def ge(a, x):
     # x以上
     i = bisect_left(a, x)
     if i != len(a):return a[i]
-    raise ValueError
+    return None
